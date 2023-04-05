3ae8de57527dd40259213fd648dce7b81bd71f0a
jjangGa98 <wlsdhks2413@naver.com>
2023-04-05 17:28:46 +0900

    이미지 추가 및 코드 수정 진행중

--- feed.js
+++ feed.js
@@ -4,12 +4,12 @@ function plusCount() {
     count++;
     document.getElementById('cnt').textContent = count;
     count--;
-    document.getElementById('follow').textContent = '팔로잉'
+    document.getElementById('follow 2').textContent = '팔로잉'
 }
 
 function minusCount() {
     count++;
     count--;
     document.getElementById('cnt').textContent = count;
-    document.getElementById('follow').textContent = '팔로우'
+    document.getElementById('follow 2').textContent = '팔로우'
 }
